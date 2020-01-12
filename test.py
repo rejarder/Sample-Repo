4cf4d4f41db99cc4a6b039c3a456a1ef5e05ac7b
rejarder <rejarder@hotmail.com>
2020-01-12 11:36:32 -0500

    committing test .py file to test compare change functionality

---
+++ test.py
@@ -0,0 +1,16 @@
+import math
+import os
+import sys
+
+# print(sys.version)
+# print(sys.executable)
+print("It worked!!!")
+
+
+def greet(whoToGreet):
+    gretting = "Hello, {}".format(whoToGreet)
+    return gretting
+
+
+x = input("Who am I greeting? ")
+print(greet(x))
